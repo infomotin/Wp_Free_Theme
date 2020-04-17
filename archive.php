c8a5fbe534623dc47db432aee55fb5d264ffd5fe
infomotin <touch.u.bc1000@gmail.com>
2020-04-17 21:45:54 +0600

    first

--- archive.php
+++ archive.php
@@ -4,9 +4,9 @@ get_header();?>
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <!-- <h1 class="page-banner__title"><?php the_title(); ?></h1> -->
-      <H1 class="page-banner__title"><?php the_archive_title( 'Wellcome ',' ' ) ?></H1>
+      <H1 class="page-banner__title"><?php the_archive_title( 'Wellcome ',' ' ); ?></H1>
       <div class="page-banner__intro">
-        <p>Repace This Text Next Time. </p>
+        <p><?php the_archive_description(); ?></p>
         <p>This Page Power by archive.php </p>
       </div>
     </div>  
