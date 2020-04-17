96b1cf0ee8d11fb9cd19799124c07de0b6ee7a11
infomotin <touch.u.bc1000@gmail.com>
2020-04-17 19:51:35 +0600

    first

--- archive.php
+++ archive.php
@@ -4,13 +4,7 @@ get_header();?>
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <!-- <h1 class="page-banner__title"><?php the_title(); ?></h1> -->
-      <H1 class="page-banner__title"><?php if (is_category( )){
-          single_cat_title( );
-      } 
-      if(is_author( )){
-        echo "Post By ";the_author();
-      }
-      ?></H1>
+      <H1 class="page-banner__title"><?php the_archive_title( 'Wellcome ',' ' ) ?></H1>
       <div class="page-banner__intro">
         <p>Repace This Text Next Time. </p>
         <p>This Page Power by archive.php </p>
