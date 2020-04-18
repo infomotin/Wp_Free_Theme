27302352a28480e228a3704cfa68abc8733aae4f
infomotin <touch.u.bc1000@gmail.com>
2020-04-18 20:08:04 +0600

    first

--- front-page.php
+++ front-page.php
@@ -24,12 +24,26 @@
                 ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month"><?php the_time( 'M' ); ?></span>
-            <span class="event-summary__day"><?php the_time( 'd' ); ?></span>  
+            <span class="event-summary__month"><?php 
+            
+            $aData = get_field('event_date');
+            //echo $aData;
+            //$eventDate  = the_field('event_date');
+            //$eventDate = new DateTime(the_field('event_date'));
+            $eventDate = new DateTime($aData);
+            echo $eventDate->format('M');
+            
+            ?></span>
+            <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink( ); ?>"><?php the_title( ); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink( ); ?>" class="nu gray">Learn more</a></p>
+            <p><?php if(has_excerpt( )){
+                      echo get_the_excerpt();
+            }else{
+              echo wp_trim_words(get_the_content(), 18);
+            } 
+            ?> <a href="<?php the_permalink( ); ?>" class="nu gray">Learn more</a></p>
           </div>
         </div>
         <!-- <div class="event-summary">
@@ -45,7 +59,7 @@
         <?php }
         wp_reset_postdata();
         ?>
-        <p class="t-center no-margin"><a href="<?php echo site_url('/event'); ?>" class="btn btn--blue">View All Events</a></p>
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') //site_url('/event');;// ?>" class="btn btn--blue">View All Events</a></p>
 
       </div>
     </div>
@@ -73,7 +87,7 @@
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink( ); ?>"><?php the_title(); ?></a></h5>
             <p><?php if(has_excerpt( )){
-                      the_excerpt();
+                      echo get_the_excerpt();
             }else{
               echo wp_trim_words(get_the_content(), 18);
             }//the_excerpt();//echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink( ); ?>" class="nu gray">Read more</a></p>
